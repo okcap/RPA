1907f19ce7532476fd560d2793e470cd6c3bf5c2
okcap <53953797+okcap@users.noreply.github.com>
2019-08-09 14:28:43 -0700

    Add files via upload

---
+++ openStep.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+yum install -y \
+libxcb.so.1 \
+libXext.so.6 \
+libdl.so.2 \
+libpthread.so.0 \
+libm.so.6 \
+libssl.so.1.1 \
+libcrypto.so.1.1.0 \
+libXau.so.6 \
+libXdmcp.so.6
---
+++ packages.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+yum install -y alsa-lib nss 
+
+yum install -y \
+libX11-xcb.so.1 \
+libfontconfig.so.1 \
+libpango-1.0.so.0 \
+libpangocairo-1.0.so.0 \
+libcairo.so.2 \
+libXcomposite.so.1 \
+libXcursor.so.1 \
+libXdamage.so.1 \
+libXfixes.so.3 \
+libXi.so.6 \
+libXrender.so.1 \
+libXtst.so.6 \
+libXrandr.so.2 \
+libXss.so.1 \
+libgconf-2.so.4 \
+libgio-2.0.so.0 \
+libasound.so.2 \
+libcups.so.2 \
+libdbus-1.so.3 \
+libatk-1.0.so.0 \
+libgtk-x11-2.0.so.0 \
+libgdk-x11-2.0.so.0 \
+libgdk_pixbuf-2.0.so.0 \
+libnspr4.so \
+libnss3.so \
+libnssutil3.so \
+libsmime3.so \
+libexpat.so.1 \
+libxcb.so.1 \
+xorg-x11-server-Xvfb
